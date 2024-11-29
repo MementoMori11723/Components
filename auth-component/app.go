a8276e8c8758fb807501e3dbdc884b7296f72614
yasasvi <yasasvigumma@gmail.com>
2024-11-29 17:35:13 +0530

    Added a auth component...
    is working...

--- auth-component/app.go
+++ auth-component/app.go
@@ -1,1 +1,81 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+var (
+	clientID     = os.Getenv("CLIENT_ID")
+	clientSecret = os.Getenv("CLIENT_SECRET")
+	redirectURI  = "http://localhost:8089/callback"
+	authURL      = "https://accounts.google.com/o/oauth2/auth"
+	tokenURL     = "https://oauth2.googleapis.com/token"
+	userInfoURL  = "https://www.googleapis.com/oauth2/v2/userinfo"
+)
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	authCodeURL := fmt.Sprintf("%s?client_id=%s&redirect_uri=%s&response_type=code&scope=email+profile",
+		authURL, clientID, redirectURI)
+	http.Redirect(w, r, authCodeURL, http.StatusFound)
+}
+
+func handleCallback(w http.ResponseWriter, r *http.Request) {
+	code := r.URL.Query().Get("code")
+	if code == "" {
+		http.Error(w, "Authorization code not found", http.StatusBadRequest)
+		return
+	}
+
+	data := url.Values{}
+	data.Set("client_id", clientID)
+	data.Set("client_secret", clientSecret)
+	data.Set("redirect_uri", redirectURI)
+	data.Set("grant_type", "authorization_code")
+	data.Set("code", code)
+
+	resp, err := http.PostForm(tokenURL, data)
+	if err != nil {
+		http.Error(w, "Failed to exchange token: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	var tokenResp struct {
+		AccessToken string `json:"access_token"`
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&tokenResp); err != nil {
+		http.Error(w, "Failed to parse token response: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	req, _ := http.NewRequest("GET", userInfoURL, nil)
+	req.Header.Set("Authorization", "Bearer "+tokenResp.AccessToken)
+	userResp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		http.Error(w, "Failed to fetch user info: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer userResp.Body.Close()
+
+	body, _ := io.ReadAll(userResp.Body)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body) 
+}
+
+func main() {
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("<a href='/login'>Login with Google</a><br/><h1>Google OAuth2.0</h1>"))
+  })
+	http.HandleFunc("/login", handleLogin)
+	http.HandleFunc("/callback", handleCallback)
+
+	fmt.Println("Server is running on http://localhost:8089...")
+	log.Fatal(http.ListenAndServe(":8089", nil))
+}
+
